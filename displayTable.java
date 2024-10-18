ee5e0aa3e746888b4b7207e4a55da77943537bdb
Sayali <119116326+Sayali-H-Thorat@users.noreply.github.com>
2024-10-18 23:13:20 +0530

    Add files via upload

---
+++ displayTable.java
@@ -0,0 +1,23 @@
+package dowhile;
+
+import java.util.Scanner;
+
+public class displayTable 
+{
+	public static void table()
+	{
+		Scanner sobj = new Scanner(System.in);
+		
+		System.out.println("Enter the number to print table:");
+		int iNo = sobj.nextInt();
+		
+		System.out.println("Table for "+iNo+" is:");
+	}
+
+	public static void main(String[] args)
+	{
+		
+
+	}
+
+}
